369ff19c50e7ee051f902f953439c3ca688b4a15
vanmer <60948275+vanmer@users.noreply.github.com>
2021-01-12 16:48:38 +0100

    start devoloping functionality

---
+++ docs/js/main.js
@@ -0,0 +1,25 @@
+import { setSearchFocus } from "./searchBar.js";
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+});
+
+const initApp = () => {
+    // set the focus
+    setSearchFocus();
+
+    // 3 listeners clear text
+
+    const form = document.getElementById("searchBar");
+    form.addEventListener("submit", submitTheSearch);
+};
+
+// Procedural "workflow" function
+const submitTheSearch = (event) => {
+    event.preventDefault();
+    // delete search results
+    // process the search
+    // set the focus
+};
